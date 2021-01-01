8d1ec168457e8d8afb9253ada2f918d515414bbe
gintautasm <>
2021-01-01 15:08:22 +0200

    small cleanup on project

--- api/ApiPaths.py
+++
@@ -1,8 +0,0 @@
-class ApiPaths(object):
-
-    _gdpPerCapita = '/gdp-per-capita'
-
-    def get_gdpPerCapita(self):
-        return type(self)._gdpPerCapita
-
-    GdpPerCapita = property(get_gdpPerCapita)
